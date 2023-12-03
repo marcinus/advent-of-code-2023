f77febf0e5886fb4d317e7057ee24051b4212fb4
Marcin Szymura <marcin.szymura@gmail.com>
2023-12-03 23:43:41 +0100

    Day 2-1

---
+++ day2-1.groovy
@@ -0,0 +1,39 @@
+sampleInput = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+""".split('\n') as List<String>
+
+def input = new File('inputs/day2-1.txt').readLines()
+
+GAME_PATTERN = /Game (?<id>\d+): (?<repeatedThrows>.*)/
+BLUE_PATTERN = ~/(\d+) blue/
+RED_PATTERN = ~/(\d+) red/
+GREEN_PATTERN = ~/(\d+) green/
+
+BLUE_MAX=14
+RED_MAX=12
+GREEN_MAX=13
+
+def solve(List<String> lines) {
+    lines.findResults { line ->
+        def match = (line =~ GAME_PATTERN)
+        assert match.matches()
+        def id = match.group('id') as int
+        def repeatedThrows = match.group('repeatedThrows')
+        def allThrows = repeatedThrows.split(';') as List
+        def feasible = allThrows.every { singleThrow ->
+            def blueMatch = (singleThrow =~ BLUE_PATTERN)
+            def redMatch = (singleThrow =~ RED_PATTERN)
+            def greenMatch = (singleThrow =~ GREEN_PATTERN)
+            def blue = blueMatch.size() == 1 ? blueMatch[0][1] as int : 0
+            def red = redMatch.size() == 1 ? redMatch[0][1] as int : 0
+            def green = greenMatch.size() == 1 ? greenMatch[0][1] as int : 0
+            blue <= BLUE_MAX && red <= RED_MAX && green <= GREEN_MAX
+        } ? id : null
+    }.sum()
+}
+
+assert 8 == solve(sampleInput)
+println solve(input)
--- day2.groovy
+++ day2.groovy
@@ -28,15 +28,13 @@ def solve(List<String> lines) {
         def numbersSpelled = NUMBERS_SPELLED.withIndex().collectEntries { it, index -> [(index): [line.indexOf(it), line.lastIndexOf(it)]] }
         def maximumSpelled = numbersSpelled.findAll { it.value[1] >= 0 }.max { it.value[1] }
         def minimumSpelled = numbersSpelled.findAll { it.value[0] >= 0 }.min { it.value[0] }
-        println numberIndexes[0]
-        //println numberIndexes.isEmpty() ? -1 : numberIndexes[-1]
-        def firstDigit = minimumSpelled.value[0] >= 0 && 
+        def firstDigit = minimumSpelled != null && minimumSpelled.value[0] >= 0 && 
         (numberIndexes.isEmpty() || minimumSpelled.value[0] < numberIndexes[0]) ? minimumSpelled.key : line[numberIndexes[0]]
-        def lastDigit = maximumSpelled.value[1] >= 0 && 
+        def lastDigit = maximumSpelled != null && maximumSpelled.value[1] >= 0 && 
         (numberIndexes.isEmpty() || maximumSpelled.value[1] > numberIndexes[-1]) ? maximumSpelled.key : line[numberIndexes[-1]]
         "${firstDigit}${lastDigit}" as int 
     }.sum()
 }
 
-assert 142 == solve(sampleInput)
+assert 281 == solve(sampleInput)
 println solve(input)
