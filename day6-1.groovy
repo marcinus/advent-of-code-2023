c6ee53dc04b0262b237f631ba52bdad3ad53a214
SzymuraM <marcin.szymura@broadridge.com>
2023-12-07 01:10:45 +0100

    Days 6-1 and 6-2

---
+++ day6-1.groovy
@@ -0,0 +1,44 @@
+import org.apache.tools.ant.util.StringUtils
+
+sampleInput = '''Time:      7  15   30
+Distance:  9  40  200
+'''.split('\n') as List<String>
+
+def input = new File('inputs/day6-1.txt').readLines()
+
+TIME = ~/Time:\s+(\d.*)$/
+DISTANCE = ~/Distance:\s+(\d.*)$/
+
+def solveSingle(long time, long distance) {
+    println time*time-4*distance
+    println Math.sqrt(time*time-4*distance)
+    double delta = Math.sqrt(time*time-4*distance)
+    long left = Math.max(0, Math.floor((time-delta)/2))
+    long right = Math.min(time, Math.ceil((time+delta)/2))
+    if(left*left - time*left + distance < 0) left -= 1
+    if(right*right - time*right + distance < 0) right += 1
+    println left
+    println right
+    return right - left - 1
+}
+
+def solve(List<String> input) {
+    def (times, distances) = parse(input)
+    (0..<(times.size())).inject(1) { cumulative, i ->
+        cumulative * solveSingle(times[i], distances[i])
+    }
+}
+
+def parse(List<String> input) {
+    def time = input[0]
+    def distance = input[1]
+    def timesMatcher = (time =~ TIME)
+    def distancesMatcher = (distance =~ DISTANCE)
+    assert timesMatcher.matches() && distancesMatcher.matches()
+    def times = timesMatcher.group(1).split(' ').findAll { it.size() > 0 }.collect { it.trim() as long }
+    def distances = distancesMatcher.group(1).split(' ').findAll { it.size() > 0 }.collect { it.trim() as long }
+    [times, distances]
+}
+
+assert 288 == solve(sampleInput)
+println solve(input)
