7728ebb07108b0af1509b0c7a5e6bfdc019f1dcf
SzymuraM <marcin.szymura@broadridge.com>
2023-12-09 16:41:26 +0100

    Day 9-2

---
+++ day9-2.groovy
@@ -0,0 +1,22 @@
+def sampleInput = '''0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45'''.split('\n') as List<String>
+
+def input = new File('inputs/day9-1.txt').readLines()
+
+def solve(List<String> input) {
+    input.sum { line ->
+        def series = (line.split(' ') as List<String>).collect {it as long }
+        def stack = new ArrayList<List<Long>>()
+        stack.push(series)
+        while(!stack.last().every { it == 0 } ) {
+            def lastSeries = stack.last()
+            def newSeries = (1..<(lastSeries.size())).collect { lastSeries[it]-lastSeries[it-1]}
+            stack.add(newSeries)
+        }
+        stack.reverse().inject(0) { cumulative, list -> list[0]-cumulative }
+    }
+}
+
+assert 2 == solve(sampleInput)
+println solve(input)
